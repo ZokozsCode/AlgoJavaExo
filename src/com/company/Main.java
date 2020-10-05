b6ee906c05c8f2475625c7475d342a7204a4c105
Angel <Angelmoreau95@gmail.com>
2020-10-05 16:55:27 +0200

    TP AlgoJavaExo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] scores = new int[7];
+        scores[0] = 13;
+        scores[1] = 34;
+        scores[2] = 45;
+        scores[3] = 23;
+        scores[4] = 64;
+        scores[5] = 11;
+        scores[6] = 21;
+     //   int score1 = scores[0];
+     //   System.out.println(score1);
+
+     //   System.out.println(scores[5]);
+        String[] score = {"13", "34", "45", "23","64","11","21"};
+        for (int i = 0; i < scores.length; i++) {
+            String s = score[i];
+            System.out.println("Scores=" + s);
+        }
+        int resultat =0 ;
+        for (int i = 0; i < scores.length; i++) {
+            resultat = resultat +scores[i];
+        }
+        resultat = resultat/ score.length;
+        System.out.println(resultat);
     }
+
+    
+
 }
